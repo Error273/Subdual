46f953c5376459c6521a92ae2c1f0df95eccf957
Error273 <cfif12349@ya.ru>
2020-12-13 15:17:05 +0500

    персонаж и его движение, сетка, слежение камеры за персонажем

---
+++ classes.py
@@ -0,0 +1,56 @@
+import pygame
+from constants import *
+
+
+class Grid:
+    # серая сетка на фоне, по которой будут выравниваться постройки.
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def render(self, surface, camera_x, camera_y):
+        for i in range(self.height):
+            for j in range(self.width):
+                pygame.draw.rect(surface, 'grey', (camera_x + j * CELL_SIZE,
+                                                   camera_y + i * CELL_SIZE,
+                                                   CELL_SIZE,
+                                                   CELL_SIZE), 1)
+
+
+class Player:
+    # главный игрок
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.going_left = False
+        self.going_right = False
+        self.going_up = False
+        self.going_down = False
+
+    def update(self):
+        if self.going_up:
+            self.y -= PLAYER_MOVEMENT_SPEED
+        if self.going_left:
+            self.x -= PLAYER_MOVEMENT_SPEED
+        if self.going_right:
+            self.x += PLAYER_MOVEMENT_SPEED
+        if self.going_down:
+            self.y += PLAYER_MOVEMENT_SPEED
+
+    def draw(self, surface, camera_x, camera_y):
+        pygame.draw.rect(surface, 'black', (camera_x + self.x,
+                                            camera_y + self.y - CELL_SIZE,
+                                            CELL_SIZE,
+                                            CELL_SIZE * 2))
+
+    def set_going_up(self, going_up):
+        self.going_up = going_up
+
+    def set_going_down(self, going_down):
+        self.going_down = going_down
+
+    def set_going_left(self, going_left):
+        self.going_left = going_left
+
+    def set_going_right(self, going_right):
+        self.going_right = going_right
---
+++ main.py
@@ -0,0 +1,67 @@
+from classes import *
+
+pygame.init()
+pygame.display.set_caption('Subdual')
+screen = pygame.display.set_mode(SIZE)
+
+running = True
+
+clock = pygame.time.Clock()
+
+grid = Grid(100, 100)
+
+player = Player(400, 300)
+
+# позиция камеры
+camera_x = 0
+camera_y = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # если нажали на крестик, выходим
+            running = False
+            continue
+        if event.type == pygame.KEYDOWN:
+            # если нажата клавиша вниз, то начинаем движение
+            if event.key == pygame.K_w:
+                player.set_going_up(True)
+            if event.key == pygame.K_a:
+                player.set_going_left(True)
+            if event.key == pygame.K_s:
+                player.set_going_down(True)
+            if event.key == pygame.K_d:
+                player.set_going_right(True)
+
+        if event.type == pygame.KEYUP:
+            # если клавиша отпущена, движение прекращаем
+            if event.key == pygame.K_w:
+                player.set_going_up(False)
+            if event.key == pygame.K_a:
+                player.set_going_left(False)
+            if event.key == pygame.K_s:
+                player.set_going_down(False)
+            if event.key == pygame.K_d:
+                player.set_going_right(False)
+    # если игрок двигается, то и камеру тоже нужно двигать за ним.
+    if player.going_left:
+        camera_x += PLAYER_MOVEMENT_SPEED
+    if player.going_right:
+        camera_x -= PLAYER_MOVEMENT_SPEED
+    if player.going_up:
+        camera_y += PLAYER_MOVEMENT_SPEED
+    if player.going_down:
+        camera_y -= PLAYER_MOVEMENT_SPEED
+
+    screen.fill(BACKGROUND_COLOR)
+    print(player.x, player.y)
+
+    # захват камерой получается при смещении каждого объекта на экране на координаты камеры
+    # поэтому для каждого объекта
+    grid.render(screen, camera_x, camera_y)
+
+    player.update()
+    player.draw(screen, camera_x, camera_y)
+
+    clock.tick(60)
+    pygame.display.flip()
